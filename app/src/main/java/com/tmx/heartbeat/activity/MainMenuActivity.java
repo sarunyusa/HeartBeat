2d8de556ab128f1b79cdc7b4e9819a54eb34f8d5
sarunyu.sak <sarunyu.sak@ascendcorp.com>
2016-08-26 21:00:03 +0700

    blah blah

---
+++ app/src/main/java/com/tmx/heartbeat/activity/MainMenuActivity.java
@@ -0,0 +1,53 @@
+package com.tmx.heartbeat.activity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.tmx.heartbeat.R;
+import com.tmx.heartbeat.dto.HeartBeatDTO;
+import com.tmx.heartbeat.util.Const;
+
+public class MainMenuActivity extends HeartBeatAbstractActivity {
+
+    private Button btnTopup;
+    private Button btnBPay;
+    private Button btnBCI;
+
+    private HeartBeatListener heartBeatButtonListener = new HeartBeatListener() {
+        @Override
+        public void onHeartBeatMessageListener(Object sender, HeartBeatDTO dto) {
+            Button btn = (Button) sender;
+            btn.setEnabled(dto.getStatus());
+            if (dto.getStatus()) {
+                btn.setBackgroundColor(ContextCompat.getColor(MainMenuActivity.this, R.color.colorPrimary));
+            } else {
+                btn.setBackgroundColor(Color.GRAY);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_main_menu);
+        initView();
+    }
+
+    protected void initView() {
+
+        btnTopup = (Button) findViewById(R.id.btnTopup);
+        btnBPay = (Button) findViewById(R.id.btnBPay);
+        btnBCI = (Button) findViewById(R.id.btnBCI);
+
+        addHeartBeat(btnTopup, Const.JOB_TOPUP, heartBeatButtonListener);
+        addHeartBeat(btnBPay, Const.JOB_BPAY, heartBeatButtonListener);
+        addHeartBeat(btnBCI, Const.JOB_BCI, heartBeatButtonListener);
+
+    }
+
+
+}
